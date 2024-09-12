86d2ac8276a3dc2b739780c8a5cdff1411323385
Tony Todorov <tonytodorov@protonmail.com>
2024-09-12 20:36:25 +0300

    Add details class

---
+++ GitHubSearchUsers/GitHubSearchUsers/Data/UserDetailsInfo.swift
@@ -0,0 +1,78 @@
+//
+//  UserDetailsInfo.swift
+//  GitHubSearchUsers
+//
+//  Created by Aurora on 4.09.24.
+//
+
+import Foundation
+
+struct UserDetailsInfo: Codable, Identifiable, Hashable {
+    let login: String
+    let id: Int
+    let nodeID: String
+    let avatarURL: String
+    let gravatarID: String
+    let url: String
+    let htmlURL: String
+    let followersURL: String
+    let followingURL: String
+    let gistsURL: String
+    let starredURL: String
+    let subscriptionsURL: String
+    let organizationsURL: String
+    let reposURL: String
+    let eventsURL: String
+    let receivedEventsURL: String
+    let type: String
+    let siteAdmin: Bool
+    let name: String?
+    let company: String?
+    let blog: String
+    let location: String?
+    let email: String?
+    let hireable: Bool?
+    let bio: String?
+    let twitterUsername: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let followers: Int
+    let following: Int
+    let createdAt: String
+    let updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+        case name
+        case company
+        case blog
+        case location
+        case email
+        case hireable
+        case bio
+        case twitterUsername = "twitter_username"
+        case publicRepos = "public_repos"
+        case publicGists = "public_gists"
+        case followers
+        case following
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
